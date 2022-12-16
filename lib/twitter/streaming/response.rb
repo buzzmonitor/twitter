18faa19fa507f5643ae6978f8ddf0bfe482d1da5
Bruno Machado <brunoalvirubro@gmail.com>
2022-12-16 10:10:01 -0300

    Some adjustments.

--- lib/twitter/streaming/response.rb
+++ lib/twitter/streaming/response.rb
@@ -2,31 +2,30 @@ require 'buftok'
 require 'http'
 require 'json'
 require 'twitter/error'
-require 'llhttp'
 
-module Twitter
-  module Streaming
-    class Response
-      # Initializes a new Response object
-      #
-      # @return [Twitter::Streaming::Response]
-      def initialize(&block)
-        @block     = block
-        @parser    = LLHttp::Parser.new(self, type: :response)
-        @tokenizer = BufferedTokenizer.new("\r\n")
-      end
+# module Twitter
+#   module Streaming
+#     class Response
+#       # Initializes a new Response object
+#       #
+#       # @return [Twitter::Streaming::Response]
+#       def initialize(&block)
+#         @block     = block
+#         @parser    = Http::Parser.new(self)
+#         @tokenizer = BufferedTokenizer.new("\r\n")
+#       end
 
-      def <<(data)
-        @parser << data
-      end
+#       def <<(data)
+#         @parser << data
+#       end
 
-      def on_body(data)
-        @tokenizer.extract(data).each do |line|
-          next if line.empty?
+#       def on_body(data)
+#         @tokenizer.extract(data).each do |line|
+#           next if line.empty?
 
-          @block.call(JSON.parse(line, symbolize_names: true))
-        end
-      end
-    end
-  end
-end
+#           @block.call(JSON.parse(line, symbolize_names: true))
+#         end
+#       end
+#     end
+#   end
+# end
